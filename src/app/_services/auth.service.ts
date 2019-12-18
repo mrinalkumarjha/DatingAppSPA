247618d57b9dab9940c6cd960c25675d9bacfe1b
mrinalkumarjha1 <mrinalk@itechcanada.com>
2019-12-18 21:27:12 +0530

    injected service in component

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -12,15 +12,16 @@ export class AuthService {
 
   // pipe allow us to chain rxjs operator to our request
   // with rxjs we take response from server transform with help of rxjs operator and pass
+  // we are not setting any header to post as content-type:application/json will be used by default by angular.
   login(model: any) {
-    this.http.post(this.baseUrl + 'login', model)
-      .pipe(
-        map((res: any) => {
-          const user = res;
-          if (user) {
-            localStorage.setItem('token', user.token);
-          }
-        })
-      )
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((res: any) => {
+        const user = res;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
   }
+
 }
