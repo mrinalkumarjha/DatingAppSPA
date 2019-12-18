2d24d653114c0fe6a6553bd615a3a30137ccda4e
mrinalkumarjha1 <mrinalk@itechcanada.com>
2019-12-18 20:08:56 +0530

    Added template form and validation to it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { NavComponent } from './nav/nav.component';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
