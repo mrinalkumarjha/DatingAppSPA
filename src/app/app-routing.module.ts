aeadaaf3c19bc16ac62a3e030bc37bec38cd57c4
mrinalkumarjha1 <mrinalk@itechcanada.com>
2019-12-23 18:57:58 +0530

    Added routing and some component in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
 
 
-const routes: Routes = [];
+// ordering is important in route
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }, // this is wildcard route if no route match then it will redirect to home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
@@ -20,7 +23,10 @@ import { AlertifyService } from './_services/alertify.service';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
