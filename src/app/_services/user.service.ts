b88de47dbf8762da04235630a4fa1f0b162f23b4
MITESH JHA <cool.miteshjha@gmail.com>
2019-12-29 00:50:10 +0530

    added service for update

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -22,7 +22,7 @@ export class UserService {
   }
 
   updateUser(id: number, user: User) {
-    return this.http.put(this.baseUrl + 'users/' + id, user)
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 
 }
