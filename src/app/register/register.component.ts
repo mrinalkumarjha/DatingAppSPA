550c34be3f8c0bdb50432dac6d960502712ac3bb
MITESH JHA <cool.miteshjha@gmail.com>
2020-01-11 13:55:39 +0530

    Registration extended in SPA

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDaterangepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,7 @@ import { BsDaterangepickerConfig } from 'ngx-bootstrap';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   // Created partial as BsDaterangepickerConfig has so many required field but we need to use one
   bsConfig: Partial<BsDaterangepickerConfig>;
@@ -20,7 +22,7 @@ export class RegisterComponent implements OnInit {
   // see cancel method i have used this to emit value;
   @Output() cancelRegister = new EventEmitter();
   constructor(private authService: AuthService, private alertify: AlertifyService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     // form using form group
@@ -32,11 +34,11 @@ export class RegisterComponent implements OnInit {
     // }, this.passwordMatchValidator
     // );
 
-    // Form using form builder
     this.bsConfig = {
       containerClass: 'theme-red'
     };
 
+       // Form using form builder
     this.createRegisterForm();
   }
 
@@ -59,14 +61,22 @@ export class RegisterComponent implements OnInit {
 
   register() {
     console.log(this.registerForm.value);
-    // console.log(this.model);
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertify.success('registration successful..');
-    // },
-    //   error => {
-    //     this.alertify.error(error);
-    //   }
-    // );
+    if (this.registerForm.valid) {
+      // following will clone registerform into empty object and
+      // then we assign it to user object
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        // once registration success login and redirect to member page
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        });
+
+      });
+    }
   }
 
   cancel() {
